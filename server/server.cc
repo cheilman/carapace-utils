8dec9707923b1176b88c2097663a41ef3affda0e
Chris Heilman <chris.heilman@gmail.com>
2022-11-01 00:38:19 -0400

    Have server building

--- server/server.cc
+++ server/server.cc
@@ -3,13 +3,51 @@
 #include <vector>
 
 #include "absl/strings/str_join.h"
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "protos/helloworld.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+class GreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext* contetx,
+        const HelloRequest* request,
+        HelloReply* reply) override {
+    reply->set_message(absl::StrCat("Hello", " ", request->name()));
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  GreeterServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
+  ServerBuilder builder;
+
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+
+  std::cout << "Server litsening on '" << server_address << "'" << std::endl;
+
+  server->Wait();
+}
 
 int main(int argc, char *argv[]) {
-  std::vector<std::string> v = {"foo", "bar", "baz"};
-  std::string s = absl::StrJoin(v, "-");
+  RunServer();
 
-  std::cout << "Joined string: " << s << std::endl;
-
-  return argc - 1;
+  return 0;
 }
 
