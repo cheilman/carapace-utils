929c86a3310bc608907dbb1f1ed6b28800188d2e
Chris Heilman <chris.heilman@gmail.com>
2023-10-10 15:32:31 -0400

    Super dumb module start

---
+++ common/modules/base_module.h
@@ -0,0 +1,8 @@
+#ifndef __CARAPACE_UTILS__COMMON__MODULES__BASE_MODULE_H__
+#define __CARAPACE_UTILS__COMMON__MODULES__BASE_MODULE_H__
+
+// Base module takes an input proto and output proto
+// abstract run method
+
+#endif
+
